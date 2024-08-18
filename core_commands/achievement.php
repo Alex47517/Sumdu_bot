13eb85d858a4a0051afd5fbcbbb1cf562c4c33e6
Alex47517 <37707465+Alex47517@users.noreply.github.com>
2024-08-19 00:31:48 +0300

    Update secret

--- core_commands/achievement.php
+++ core_commands/achievement.php
@@ -1,6 +1,6 @@
 <?php
 //Secret file - дійсний тільки у приватних повідомленнях з ботом
-if ($promo_code_hdje93 == 'Lfjiowfi39w') {
+if ($promo_code_hdje93 == 'Lfjiowfi639w') {
 //User::give('achievement', 'GH');
 die();
 }
